b40af832212c360ee3de1cfcc5489b9542be7322
Septian Maulana <septiandev028@gmail.com>
2024-08-04 16:29:01 +0700

    feat(web3): setup wagmi config, update next.js and create connect wallet modal
    - Created wagmi config for web3 integration
    - Updated Next.js config for compatibility
    - Created context for web3 modal
    - Developed shared header component
    - Integrated context and header into layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { ModeToggle } from "@/components/shared/mode-toggle";
-
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <ModeToggle />
     </main>
   );
 }
